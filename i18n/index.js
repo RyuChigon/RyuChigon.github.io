e1852c42666f13f9e85e7c033edbf1dbf468cee9
RyuChigon <fbclrhs10@gmail.com>
2022-07-15 20:24:26 +0900

    feat: locale for index

---
+++ i18n/index.js
@@ -0,0 +1,11 @@
+const ko = require("./locales/locale.ko.json");
+const en = require("./locales/locale.en.json");
+
+module.exports = {
+  translations: {
+    ko,
+    en,
+  },
+  defaultLang: "ko",
+  useBrowserDefault: true,
+}
