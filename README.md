6508fb007865fb7adb3c941fa9d57890f08265ea
RyuChigon <fbclrhs10@gmail.com>
2023-02-22 22:34:36 +0900

    deploy

--- README.md
+++
@@ -1,5 +0,0 @@
-# 개인 블로그
-
-url: https://RyuChigon.github.io
-
-`Next.js`를 이용한 개인 블로그 만들기 프로젝트
--- pages/blog/index.jsx
+++
@@ -1,4 +0,0 @@
-import { getStaticProps } from "./page/[page]";
-export { getStaticProps }
-import BlogPage from "./page/[page]";
-export default BlogPage;
